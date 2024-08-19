f7480b0fee2dbcefe70d3f0d6a262b71e30c0a95
smedvedeva2412 <smedvedeva2412@gmail.com>
2024-08-19 20:55:03 +0200

    query_tabel_created

--- query_templates.py
+++ query_templates.py
@@ -1,5 +1,3 @@
-#SQL queries
-
 # Поиск фильмов по ключевому слову
 search_by_keyword_query = """
 SELECT title, description
@@ -23,4 +21,21 @@ JOIN
 WHERE 
     c.name = %s AND f.release_year = %s
 LIMIT 10;
+"""
+
+# Создание таблицы для хранения поисковых запросов
+create_search_keywords_table_query = """
+CREATE TABLE IF NOT EXISTS search_keywords_sv (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    keyword VARCHAR(255) NOT NULL,
+    search_count INT DEFAULT 1,
+    last_search DATETIME DEFAULT NOW()
+);
+"""
+
+# Сохранение поискового запроса или обновление счетчика и времени поиска
+save_search_query = """
+INSERT INTO search_keywords_sv (keyword, search_count)
+VALUES (%s, 1)
+ON DUPLICATE KEY UPDATE search_count = search_count + 1, last_search = NOW();
 """
