c423dcde24ed82f2b1aec54390664d859b6c748a
smedvedeva2412 <smedvedeva2412@gmail.com>
2024-08-19 16:42:59 +0200

    Local added

--- local_settings.py
+++ local_settings.py
@@ -2,3 +2,10 @@ HOST = 'ich-db.ccegls0svc9m.eu-central-1.rds.amazonaws.com'
 USER = 'ich1'
 PASSWORD = 'password'
 DATABASE = 'sakila'
+
+dbconfig = {
+    'host': HOST,
+    'user': USER,
+    'password': PASSWORD,
+    'database': DATABASE,
+}
