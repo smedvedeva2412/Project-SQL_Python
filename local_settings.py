1960dbdc4c24c64e9a2a9200d61bd0e60190be5d
smedvedeva2412 <smedvedeva2412@gmail.com>
2024-08-21 12:35:45 +0200

    connection changed

--- local_settings.py
+++ local_settings.py
@@ -1,7 +1,7 @@
-HOST = 'ich-db.ccegls0svc9m.eu-central-1.rds.amazonaws.com'
+HOST = 'mysql.itcareerhub.de'
 USER = 'ich1'
-PASSWORD = 'password'
-DATABASE = 'sakila'
+PASSWORD = 'ich1_password_ilovedbs'
+DATABASE = 'sakila_Sveta'
 
 dbconfig = {
     'host': HOST,
